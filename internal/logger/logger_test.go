86191e500349b29448abd374d0824b2e76e6d817
Lucas Soares <lucas@stilingue.com.br>
2023-06-23 14:45:44 -0300

    test: add json logging base configuration test

--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -25,6 +25,21 @@ func TestConfigureLoggerWithoutDebug(t *testing.T) {
 	require.Equal(t, false, config.DebugEnabled.GetBool())
 }
 
+func TestConfigureLoggerWithJson(t *testing.T) {
+	config.LogType.Set("json")
+	ConfigureLogger()
+
+	require.Equal(t, "json", config.LogType.Get())
+
+}
+
+func TestConfigureLoggerWithoutJson(t *testing.T) {
+	config.LogType.Set("console")
+	ConfigureLogger()
+
+	require.Equal(t, "console", config.LogType.Get())
+}
+
 func TestLoggerS(t *testing.T) {
 	S(context.Background())
 }
